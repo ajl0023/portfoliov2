212fd1ed8586c66ced7e25f27b51c774b7d1c028
austin <austinlee199723@yahoo.com>
2021-06-09 00:36:16 -0700

    fixed navbar

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -38,71 +38,69 @@ export const renderLanding = (fragment) => {
   element.innerHTML = /*html*/ `
 <div class=${style["center-container"]}>
 
-    <div class=${style["container"]}>
-      <div class=${style["text-container"]}>
-        <h1 class=${style["main-title"]}>Hi, I'm <span>Austin</span></h1>
-        <h4 class=${style["description"]}>
-          Here are some of my projects I've been working on.
-        </h4>
-      </div>
-      <div class=${style["image-container"]}>
-        <a
-          href="https://github.com/ajl0023/chatApp"
-          target="_blank"
-          class=${style["project-container"]}
-        >
+  <div class=${style["container"]}>
+    <div class=${style["text-container"]}>
+      <h1 class=${style["main-title"]}>Hi, I'm <span>Austin</span></h1>
+      <h4 class=${style["description"]}>
+        Here are some of my projects I've been working on.
+      </h4>
+    </div>
+    <div class=${style["image-container"]}>
+      <button class=${style["project-container"]}>
+        <a href="https://github.com/ajl0023/chatApp" target="_blank" >
           <img src=${chatapp} class=${style["project-image"]} />
         </a>
-        <a
-          href="https://github.com/ajl0023/Covid-tracker"
-          target="_blank"
-          class=${style["project-container"]}
-        >
+      </button>
+
+      <button class=${style["project-container"]}>
+        <a href="https://github.com/ajl0023/Covid-tracker" target="_blank" class=${
+          style["project-container"]
+        }>
           <img src=${covidTracker} class=${style["project-image"]} />
         </a>
-        <a
-          href="https://github.com/ajl0023/readditv2"
-          target="_blank"
-          class=${style["project-container"]}
-        >
+      </button>
+      <button class=${style["project-container"]}>
+        <a href="https://github.com/ajl0023/readditv2" target="_blank" class=${
+          style["project-container"]
+        }>
           <img src=${readdit} class=${style["project-image"]} />
         </a>
-        <a
-          href="https://github.com/ajl0023/spotifyPlaylists"
-          target="_blank"
-          class=${style["project-container"]}
-        >
+      </button>
+      <button class=${style["project-container"]}>
+        <a href="https://github.com/ajl0023/spotifyPlaylists" target="_blank" class=${
+          style["project-container"]
+        }>
           <img src=${playlists} class=${style["project-image"]} />
         </a>
-        <a
-          href="https://github.com/ajl0023/teslaClone"
-          target="_blank"
-          class=${style["project-container"]}
-        >
+      </button>
+      <button class=${
+        style["project-container"]
+      }><a href="https://github.com/ajl0023/teslaClone" target="_blank" class=${
+    style["project-container"]
+  }>
           <img src=${tesla} class=${style["project-image"]} />
+        </a></button>
+
+      <button class=${
+        style["project-container"]
+      }><a href="https://github.com/ajl0023/tftapp2" target="_blank" class=${
+    style["project-container"]
+  }>
+          <img src=${tftapp} class=${`${style["project-image"]} ${style["portfolio-image"]}`} />
         </a>
-        <a
-          href="https://github.com/ajl0023/tftapp2"
-          target="_blank"
-          class=${style["project-container"]}
-        >
-          <img
-            src=${tftapp}
-            class=${`${style["project-image"]} ${style["portfolio-image"]}`}
-          />
-        </a>
-      </div>
-      
+      </button>
+    </div>
+
   </div>
   <div class=${style["tech-wrapper"]}>
-          <p class=${style["tech-container-title"]}>
-            Technologies used in these projects
-          </p>
-          <div id='tech-container' class=${style["tech-icon-container"]}>
-            
-          </div>
-        </div>
-    </div>`;
+    <p class=${style["tech-container-title"]}>
+      Technologies used in these projects
+    </p>
+    <div id='tech-container' class=${style["tech-icon-container"]}>
+
+    </div>
+  </div>
+</div>`;
   const root = fragment.getElementById("#root");
   root.appendChild(element);
   const techContainer = fragment.getElementById("tech-container");
